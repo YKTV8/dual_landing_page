00155013461d49a2b3ec3fdb0826bffd8b709c25
YKTV8 <97923330+YKTV8@users.noreply.github.com>
2023-03-14 22:27:29 -0700

    test

--- Web Dev/assets/php/email.php
+++ Web Dev/assets/php/email.php
@@ -1,68 +1,46 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/main.css">
+</head>
+<body>
+    <?php
+    if(!empty($_POST["send"])){
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $message = $_POST["message"];
+        $toEmail = $_POST["gmatthews2787@outlook.com"];
+
+        $mailHeaders = "Name: " . $name . 
+        "\r\n Email: " . $email . 
+        "r\n\ Message: " . $message .;
+
+        if(mail($toEmail, $name, $mailHeaders)){
+            $message = "Your info has been submitted and received. Thank you"
+
+        }
 
 
-if(isset($_POST['email'])) {
-  
-  // EDIT THE FOLLOWING LINES AS REQUIRED
-  $email_to = "gmatthews2787@outlook.com";
-  $email_subject = "New Contact Form Submission";
-  
-  function died($error) {
-    // your error code can go here
-    echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-    echo "These errors appear below.<br /><br />";
-    echo $error."<br /><br />";
-    echo "Please go back and fix these errors.<br /><br />";
-    die();
-  }
-  
-  // validation expected data exists
-  if(!isset($_POST['name']) ||
-    !isset($_POST['email']) ||
-    !isset($_POST['message'])) {
-    died('We are sorry, but there appears to be a problem with the form you submitted.');       
-  }
-  
-  $name = $_POST['name']; // required
-  $email_from = $_POST['email']; // required
-  $message = $_POST['message']; // required
-  
-  $error_message = "";
-  $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/';
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
-  $string_exp = "/^[A-Za-z .'-]+$/";
-  if(!preg_match($string_exp,$name)) {
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
-  }
-  if(strlen($message) < 2) {
-    $error_message .= 'The Message you entered do not appear to be valid.<br />';
-  }
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
-  
-  $email_message = "Form details below.\n\n";
-  
-  function clean_string($string) {
-    $bad = array("content-type","bcc:","to:","cc:","href");
-    return str_replace($bad,"",$string);
-  }
-  
-  $email_message .= "Name: ".clean_string($name)."\n";
-  $email_message .= "Email: ".clean_string($email_from)."\n";
-  $email_message .= "Message: ".clean_string($message)."\n";
-  
-  // create email headers
-  $headers = 'From: '.$email_from."\r\n".
-  'Reply-To: '.$email_from."\r\n" .
-  'X-Mailer: PHP/' . phpversion();
-  @mail($email_to, $email_subject, $email_message, $headers); 
-} 
-?>
-  
-  <!-- include your own success html here -->
-  <div class="thank-you-message">
-    Thank you for contacting us. We will be in touch with you very soon.
-  </div>
+    }
+    ?>
+    <main>
+        <p>send Email</p>
+        <form method="post" name="emailContact">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" >
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" >
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
+            <!-- <button type="submit">Submit</button> -->
+            <input type="submit" name="send" value="Submitttttt">
+            <?php if(!empty($message)){ echo $message;
+                 } ?>
+          </form>
+    </main>
+</body>
+</html>
